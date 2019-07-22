ac10300d40e19cc7380634d5c02e369328981757
Fernando Piancastelli <fernando@rethink.ws>
2019-07-22 16:29:49 -0300

    Several pt-BR translations

--- resources/lang/pt-BR/changelog.php
+++ resources/lang/pt-BR/changelog.php
@@ -1,6 +1,6 @@
 <?php
 
 return [
-    'title' => 'Product changes',
-    'note' => 'Note: unfortunately, this page is only in English.',
+    'title' => 'Alterações do produto',
+    'note' => 'Obs: infelizmente, esta página se encontra apenas em Inglês.',
 ];
--- resources/lang/pt-BR/dashboard.php
+++ resources/lang/pt-BR/dashboard.php
@@ -1,36 +1,36 @@
 <?php
 
 return [
-    'dashboard_blank_title' => 'Welcome to your account!',
-    'dashboard_blank_description' => 'Monica is the place to organize all the interactions you have with the ones you care about.',
-    'dashboard_blank_cta' => 'Add your first contact',
-    'dashboard_blank_illustration' => 'Illustration by <a href=":url">Freepik</a>',
+    'dashboard_blank_title' => 'Bem-vindo à sua conta!',
+    'dashboard_blank_description' => 'Monica é o lugar para organizar todas as interações que você tem com quem você se importa.',
+    'dashboard_blank_cta' => 'Adicionar seu primeiro contato',
+    'dashboard_blank_illustration' => 'Ilustrações por <a href=":url">Freepik</a>',
 
-    'notes_title' => 'You don’t have any starred notes yet.',
+    'notes_title' => 'Você não tem nenhuma observação com estrela.',
 
-    'tab_recent_calls' => 'Recent calls',
-    'tab_favorite_notes' => 'Favorite notes',
-    'tab_calls_blank' => 'You haven’t logged a call yet.',
-    'tab_debts' => 'Debts',
-    'tab_debts_blank' => 'You haven’t logged any debt yet.',
-    'tab_tasks' => 'Tasks',
-    'tab_tasks_blank' => 'You haven’t any task yet.',
+    'tab_recent_calls' => 'Ligações recentes',
+    'tab_favorite_notes' => 'Observações favoritas',
+    'tab_calls_blank' => 'Você não registrou uma ligação ainda.',
+    'tab_debts' => 'Débitos',
+    'tab_debts_blank' => 'Você não registrou nenhum débito ainda.',
+    'tab_tasks' => 'Tarefas',
+    'tab_tasks_blank' => 'Você ainda não tem tarefas.',
 
-    'tasks_add_task_placeholder' => 'What is this task about?',
-    'tasks_tab_your_contacts' => 'Tasks related to your contacts',
-    'tasks_tab_your_tasks' => 'Your tasks',
-    'tasks_add_note' => 'Press <kbd>Enter</kbd> to add the task.',
-    'task_add_cta' => 'Add a task',
+    'tasks_add_task_placeholder' => 'Sobre o que se trata esta tarefa?',
+    'tasks_tab_your_contacts' => 'Tarefas relacionadas a seus contatos',
+    'tasks_tab_your_tasks' => 'Suas tarefas',
+    'tasks_add_note' => 'Aperte <kbd>Enter</kbd> para adicionar a tarefa.',
+    'task_add_cta' => 'Adicionar uma tarefa',
 
-    'debts_you_owe' => 'You owe',
+    'debts_you_owe' => 'Você deve',
 
-    'statistics_contacts' => 'Contacts',
-    'statistics_activities' => 'Activities',
-    'statistics_gifts' => 'Gifts',
+    'statistics_contacts' => 'Contatos',
+    'statistics_activities' => 'Atividades',
+    'statistics_gifts' => 'Presentes',
 
-    'reminders_next_months' => 'Events in the next 3 months',
-    'reminders_none' => 'No reminder for this month',
+    'reminders_next_months' => 'Eventos nos próximos 3 meses',
+    'reminders_none' => 'Sem lembretes para este mês',
 
-    'product_changes' => 'Product changes',
-    'product_view_details' => 'View details',
+    'product_changes' => 'Alterações de produto',
+    'product_view_details' => 'Ver detalhes',
 ];
